ffabd57df526d2bf0063f158d901b892acbc33b8
Amy Schremp <amyschremp@live.com>
2018-04-09 22:05:57 -0500

    fixed bug with deleting blog posts

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, session, flash
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -6,6 +6,7 @@ app.config['DEBUG']= True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:blogging@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO']= True
 db = SQLAlchemy(app)
+app.secret_key = '4Dm585aTpt'
 
 
 class Blog(db.Model):
@@ -20,27 +21,33 @@ class Blog(db.Model):
 
 @app.route('/')
 def index():
-    # stuck here -- having trouble figure out how to query
-    # individual parts of the blog entry. want to display
-    # title and body in an html template but am not sure what
-    # else to write out other than blog.query.all()
     blog_entries = Blog.query.all()
-    return render_template('blog.html', title = "Blog", blog_entries = blog_entries)
+    return render_template('blog.html', title = "Build-A-Blog", blog_entries = blog_entries)
 
 @app.route('/newpost', methods=['GET', 'POST'])
 def newpost():
-    entries = Blog.query.all()
     if request.method == 'POST':
-        blog_title = request.form['blog_title']
-        blog_content = request.form['blog_content']
-        new_blog = Blog(blog_title, blog_content)
-        db.session.add(new_blog)
-        db.session.commit()
-        new_blog_id = new_blog.id
-        
-        return redirect('/')
+        entry_title = request.form['blog_title']
+        entry_body = request.form['blog_content']
+        if not entry_title:
+            flash('Please enter a title.')
+        if not entry_body:
+            flash('Please enter a body.')
+        else:
+            new_blog = Blog(entry_title, entry_body)
+            db.session.add(new_blog)
+            db.session.commit()
+            new_blog_id = new_blog.id
+            return redirect('/')
     
-    return render_template('new_post.html', title = "New Blog Entry", entries = entries)
+    return render_template('new_post.html', title = "New Blog Entry")
+
+@app.route('/delete', methods=['POST'])
+def delete():
+    to_be_deleted = request.form['id']
+    Blog.query.filter_by(id=to_be_deleted).delete()
+    db.session.commit()
+    return redirect('/')
 
 if __name__ == "__main__":
     app.run()
